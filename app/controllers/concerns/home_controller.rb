814f0f40d60d241568bdf75309913b36fb509466
Alyssa <alyssaw90@gmail.com>
2015-01-11 19:58:20 -0800

    initial commit and updated files

---
+++ app/controllers/concerns/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+def index
+    # @greeting = "good morning"
+    # render plain: "hello"
+end
+
+end
---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,43 @@
+class CreaturesController < ApplicationController
+
+  def index
+    @creatures = Creature.all
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    @creature = Creature.find(params[:id])
+    @creature.update(creature_params)
+    redirect_to @creature
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+
+    @creature.save
+    redirect_to "/creatures"
+  end
+
+  def destroy
+    @creature = Creature.find(params[:id])
+    @creature.destroy
+    redirect_to creatures_path
+  end
+
+  private
+    def creature_params
+      params.require(:creature).permit(:name, :desc)
+    end
+
+end
